7a5f48b1abd568a1c28b3b4a25fc672911a1aed3
DEBANGAN MITRA <70383347+DEBANGAN18@users.noreply.github.com>
2022-07-22 01:07:38 +0530

    Update tarpping rainwater.cpp

--- tarpping rainwater.cpp
+++ tarpping rainwater.cpp
@@ -38,3 +38,40 @@ public:
 
 //T.C: O(N)
 //S.C: 0(N+N)
+
+
+
+//optimal solution : using 2pointers
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int maxl = -1;
+        int maxr = -1;
+        int n = height.size();
+        int s=0, e=n-1;
+        int ans=0;
+        while(s<=e){
+            
+            //ensures that there is building in the right which has a minimum height of height[s], if so, then check if its greater then topmost left building,
+            //then update height of topmost left building else add the volume
+            if(height[s]<=height[e]){
+                if(maxl<=height[s]) maxl=height[s];
+                else ans+=maxl-height[s];
+                s++;
+            }
+            
+            //similar to if portion but from end side.
+            else{
+                if(maxr<=height[e]) maxr=height[e];
+                else ans+=maxr-height[e];
+                e--;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//T.C: O(N)
+//S.C: 0(1)
+
